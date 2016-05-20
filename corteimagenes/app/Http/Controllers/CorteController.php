e1a6969e506f94e1fcef5cd0a8430f72e7a0ec75
luguiant <mauriciogmona@gmail.com>
2016-05-20 16:24:41 -0500

    se agregan aclaraciones al codigo

--- corteimagenes/app/Http/Controllers/CorteController.php
+++ corteimagenes/app/Http/Controllers/CorteController.php
@@ -15,10 +15,10 @@ class CorteController extends Controller
     public function saveimage(Request $request){
     	if($request->isMethod('post'))
 		{
-			$targ_w = $targ_h = 150;
-			$jpeg_quality = 90;
+			$targ_w = $targ_h = 150; // dimensiones para el resize
+			$jpeg_quality = 90; //  calidad de la imagen 
 
-			$src = url('/').'/demos/demo_files/pool.jpg';
+			$src = url('/').'/demos/demo_files/pool.jpg'; //origen de la imagen 
 			$img_r = imagecreatefromjpeg($src);
 			$dst_r = ImageCreateTrueColor( $targ_w, $targ_h );
 
